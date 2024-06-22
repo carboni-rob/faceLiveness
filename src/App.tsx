1ce188ae1e790da15915936079375553ad14bc96
Roberto Carboni <carboni@robertocarboni.it>
2024-06-22 11:40:31 +0100

    first face liveness implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,67 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import React from "react";
+import { Loader, ThemeProvider } from "@aws-amplify/ui-react";
+// import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import awsexports from "./aws-exports";
+import { FaceLivenessDetector } from "@aws-amplify/ui-react-liveness";
 
-const client = generateClient<Schema>();
+// Amplify.configure(awsexports);
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+export default function App() {
+  const [loading, setLoading] = React.useState(true);
+  const [createLivenessApiData, setCreateLivenessApiData] = React.useState(null);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+  React.useEffect(() => {
+    const fetchCreateLiveness = async () => {
+      /*
+       * This should be replaced with a real call to your own backend API
+       */
+      await new Promise((r) => setTimeout(r, 2000));
+      const mockResponse = { sessionId: "mockSessionId" };
+      const data = mockResponse;
+
+      setCreateLivenessApiData(data);
+      setLoading(false);
+    };
+
+    fetchCreateLiveness();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  const handleAnalysisComplete = async () => {
+    /*
+     * This should be replaced with a real call to your own backend API
+     */
+    const response = await fetch(`/api/get?sessionId=${createLivenessApiData.sessionId}`);
+    const data = await response.json();
+
+    /*
+     * Note: The isLive flag is not returned from the GetFaceLivenessSession API
+     * This should be returned from your backend based on the score that you
+     * get in response. Based on the return value of your API you can determine what to render next.
+     * Any next steps from an authorization perspective should happen in your backend and you should not rely
+     * on this value for any auth related decisions.
+     */
+    if (data.isLive) {
+      console.log("User is live");
+    } else {
+      console.log("User is not live");
+    }
+  };
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <main>
-          <h1>My todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id}>{todo.content}</li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
+    <ThemeProvider>
+      {loading ? (
+        <Loader />
+      ) : (
+        <FaceLivenessDetector
+          sessionId={createLivenessApiData.sessionId}
+          region="us-east-1"
+          onAnalysisComplete={handleAnalysisComplete}
+          onError={(error) => {
+            console.error(error);
+          }}
+        />
       )}
-    </Authenticator>
+    </ThemeProvider>
   );
 }
-
-export default App;
